b16c44007d7d4a45de46ff82081c0eebec711fbd
Toomas Soome <tsoome@me.com>
2018-10-21 22:48:51 +0300

    usba: cast between incompatible function types
    ../../common/io/usb/usba/usbai.c: In function 'usb_register_hotplug_cbs':
    ../../common/io/usb/usba/usbai.c:1042:6: error: cast between incompatible function types from 'int (*)(struct __dev_info **)' to 'void (*)(struct __dev_info **, struct ddi_event_cookie *, void *, void *)' [-Werror=cast-function-type]
          (peh_t)disconnect_event_handler,
          ^
    ../../common/io/usb/usba/usbai.c:1060:6: error: cast between incompatible function types from 'int (*)(struct __dev_info **)' to 'void (*)(struct __dev_info **, struct ddi_event_cookie *, void *, void *)' [-Werror=cast-function-type]
          (peh_t)reconnect_event_handler,
          ^
    ../../common/io/usb/usba/usbai.c: In function 'usb_register_event_cbs':
    ../../common/io/usb/usba/usbai.c:1131:7: error: cast between incompatible function types from 'int (*)(struct __dev_info **)' to 'void (*)(struct __dev_info **, struct ddi_event_cookie *, void *, void *)' [-Werror=cast-function-type]
           (peh_t)usb_evdata->disconnect_event_handler,
           ^
    ../../common/io/usb/usba/usbai.c:1146:7: error: cast between incompatible function types from 'int (*)(struct __dev_info **)' to 'void (*)(struct __dev_info **, struct ddi_event_cookie *, void *, void *)' [-Werror=cast-function-type]
           (peh_t)usb_evdata->reconnect_event_handler,
           ^
    ../../common/io/usb/usba/usbai.c:1161:7: error: cast between incompatible function types from 'int (*)(struct __dev_info **)' to 'void (*)(struct __dev_info **, struct ddi_event_cookie *, void *, void *)' [-Werror=cast-function-type]
           (peh_t)usb_evdata->post_resume_event_handler,
           ^
    ../../common/io/usb/usba/usbai.c:1176:7: error: cast between incompatible function types from 'int (*)(struct __dev_info **)' to 'void (*)(struct __dev_info **, struct ddi_event_cookie *, void *, void *)' [-Werror=cast-function-type]
           (peh_t)usb_evdata->pre_suspend_event_handler,
           ^
    cc1: all warnings being treated as errors

--- usr/src/uts/common/io/usb/usba/usbai.c
+++ usr/src/uts/common/io/usb/usba/usbai.c
@@ -1039,7 +1039,7 @@ usb_register_hotplug_cbs(dev_info_t *dip,
 		}
 	}
 	if (ddi_add_event_handler(dip, usba_device->rm_cookie,
-	    (peh_t)disconnect_event_handler,
+	    (peh_t)(uintptr_t)disconnect_event_handler,
 	    NULL, &evdata->ev_rm_cb_id) != DDI_SUCCESS) {
 		USB_DPRINTF_L2(DPRINT_MASK_USBAI, usbai_log_handle,
 		    "usb_register_hotplug_cbs: add disconnect handler failed");
@@ -1057,7 +1057,7 @@ usb_register_hotplug_cbs(dev_info_t *dip,
 		}
 	}
 	if (ddi_add_event_handler(dip, usba_device->ins_cookie,
-	    (peh_t)reconnect_event_handler,
+	    (peh_t)(uintptr_t)reconnect_event_handler,
 	    NULL, &evdata->ev_ins_cb_id) != DDI_SUCCESS) {
 		USB_DPRINTF_L2(DPRINT_MASK_USBAI, usbai_log_handle,
 		    "usb_register_hotplug_cbs: add reconnect handler failed");
@@ -1128,7 +1128,7 @@ usb_register_event_cbs(dev_info_t *dip, usb_event_t *usb_evdata,
 			}
 		}
 		if (ddi_add_event_handler(dip, usba_device->rm_cookie,
-		    (peh_t)usb_evdata->disconnect_event_handler,
+		    (peh_t)(uintptr_t)usb_evdata->disconnect_event_handler,
 		    NULL, &evdata->ev_rm_cb_id) != DDI_SUCCESS) {
 
 			goto fail;
@@ -1143,7 +1143,7 @@ usb_register_event_cbs(dev_info_t *dip, usb_event_t *usb_evdata,
 			}
 		}
 		if (ddi_add_event_handler(dip, usba_device->ins_cookie,
-		    (peh_t)usb_evdata->reconnect_event_handler,
+		    (peh_t)(uintptr_t)usb_evdata->reconnect_event_handler,
 		    NULL, &evdata->ev_ins_cb_id) != DDI_SUCCESS) {
 
 			goto fail;
@@ -1158,7 +1158,7 @@ usb_register_event_cbs(dev_info_t *dip, usb_event_t *usb_evdata,
 			}
 		}
 		if (ddi_add_event_handler(dip, usba_device->resume_cookie,
-		    (peh_t)usb_evdata->post_resume_event_handler,
+		    (peh_t)(uintptr_t)usb_evdata->post_resume_event_handler,
 		    NULL, &evdata->ev_resume_cb_id) != DDI_SUCCESS) {
 
 			goto fail;
@@ -1173,7 +1173,7 @@ usb_register_event_cbs(dev_info_t *dip, usb_event_t *usb_evdata,
 			}
 		}
 		if (ddi_add_event_handler(dip, usba_device->suspend_cookie,
-		    (peh_t)usb_evdata->pre_suspend_event_handler,
+		    (peh_t)(uintptr_t)usb_evdata->pre_suspend_event_handler,
 		    NULL, &evdata->ev_suspend_cb_id) != DDI_SUCCESS) {
 
 			goto fail;
